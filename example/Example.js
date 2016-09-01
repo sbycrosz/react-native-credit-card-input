d34c757cc3f9e59fb33574401c12efd37d915573
Sam Aryasa <sambya@aryasa.net>
2016-09-01 14:00:40 +0800

    Add initial API

--- example/Example.js
+++ example/Example.js
@@ -14,14 +14,48 @@ const s = StyleSheet.create({
     alignItems: "center",
     marginTop: 100,
   },
+  label: {
+    color: "black",
+    fontSize: 16,
+  },
+  input: {
+    color: "darkgray",
+    fontSize: 14,
+  },
+  placeholder: {
+    color: "gray",
+  },
+  error: {
+    color: "red",
+  },
 });
 
-
 export default class Example extends Component {
+  _onChange = formData => {
+    console.log(formData);
+    // {
+    //   type: "visa",
+    //   valid: true,
+    //   number: "4242424242424242"
+    //   expiryYear: 12,
+    //   expiryMonth: 4,
+    //   cvc: 200
+    // }
+  };
+
   render() {
     return (
       <View style={s.container}>
-        <CreditCardForm />
+        <CreditCardForm
+            autoFocus
+            skipName
+
+            labelStyle={s.label}
+            inputStyle={s.input}
+            placeholderStyle={s.placeholder}
+            errorStyle={s.error}
+
+            onChange={this._onChange} />
       </View>
     );
   }
