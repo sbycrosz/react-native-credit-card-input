54380b77737e220a6eec1cf3bfb4652b45d38fd5
Sam Aryasa <sambya@aryasa.net>
2016-09-05 18:22:38 +0800

    Prevent card number input from disappearing when its container is shrunk

--- src/LiteCreditCardInput.js
+++ src/LiteCreditCardInput.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import ReactNative, {
   View,
   Text,
+  TextInput,
   StyleSheet,
   Image,
   LayoutAnimation,
@@ -13,21 +14,32 @@ import CCInput from "./CCInput";
 import { removeNonNumber } from "./Utilities";
 import { InjectedProps } from "./connectToState";
 
+const INFINITE_WIDTH = 1000;
+
 const s = StyleSheet.create({
   container: {
     paddingLeft: 10,
     paddingRight: 10,
     flexDirection: "row",
     alignItems: "center",
+    overflow: "hidden",
   },
   icon: {
     width: 48,
     height: 40,
     resizeMode: "contain",
   },
-  number: {
+  expanded: {
+    flex: 1,
+  },
+  hidden: {
+    width: 0,
+  },
+  leftPart: {
+    overflow: "hidden",
   },
   rightPart: {
+    overflow: "hidden",
     flexDirection: "row",
     marginLeft: 10,
   },
@@ -35,11 +47,14 @@ const s = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
   },
-  expiry: {
-    width: 90,
+  numberInput: {
+    width: INFINITE_WIDTH,
   },
-  cvc: {
-    width: 90,
+  expiryInput: {
+    width: 80,
+  },
+  cvcInput: {
+    width: 80,
   },
 });
 
@@ -74,7 +89,7 @@ export default class LiteCreditCardInput extends Component {
   _inputProps = field => {
     const {
       inputStyle, validColor, invalidColor, placeholderColor,
-      placeholders,  values, status,
+      placeholders, values, status,
       onFocus, onChange, onBecomeEmpty, onBecomeValid
     } = this.props;
 
@@ -82,7 +97,6 @@ export default class LiteCreditCardInput extends Component {
       inputStyle, validColor, invalidColor, placeholderColor,
       ref: field, field: field,
 
-      label: "",
       placeholder: placeholders[field],
       value: values[field],
       status: status[field],
@@ -105,28 +119,32 @@ export default class LiteCreditCardInput extends Component {
 
     return (
       <View style={s.container}>
-        <View style={[s.number,
-          showRightPart ? { width: 1 } : { flex: 1 } ]}>
-          <CCInput {...this._inputProps("number")} />
+        <View style={[
+          s.leftPart,
+          showRightPart ? s.hidden : s.expanded,
+        ]}>
+          <CCInput {...this._inputProps("number")}
+              containerStyle={s.numberInput} />
         </View>
         <Image style={s.icon}
             source={{ uri: Icons[this._iconToShow()] }} />
         <View style={[
           s.rightPart,
-          showRightPart ? { flex: 1 } : { width: 1 }
+          showRightPart ? s.expanded : s.hidden,
         ]}>
           <TouchableOpacity onPress={this._focusNumber}
               style={s.last4}>
-            <Text style={[inputStyle, { height: null }]}>
-              { number.substr(number.length - 4, 4) }
-            </Text>
+            <View pointerEvents={"none"}>
+              <TextInput editable={false}
+                  underlineColorAndroid={"transparent"}
+                  style={[inputStyle]}
+                  value={number.substr(number.length - 4, 4)} />
+            </View>
           </TouchableOpacity>
-          <View style={s.expiry}>
-            <CCInput {...this._inputProps("expiry")} />
-          </View>
-          <View style={s.cvc}>
-            <CCInput {...this._inputProps("cvc")} />
-          </View>
+          <CCInput {...this._inputProps("expiry")}
+              containerStyle={s.expiryInput} />
+          <CCInput {...this._inputProps("cvc")}
+              containerStyle={s.cvcInput} />
         </View>
       </View>
     );
