881298283af88e2a6866f6312e7a9d53cb1bf790
CooLSeS <coolses@mail.ru>
2017-01-20 14:24:47 +0300

    keyboardShouldPersistTaps true/false deprecated

--- src/CreditCardInput.js
+++ src/CreditCardInput.js
@@ -160,7 +160,7 @@ export default class CreditCardInput extends Component {
             cvc={cvc} />
         <ScrollView ref="Form"
             horizontal
-            keyboardShouldPersistTaps
+            keyboardShouldPersistTaps="always"
             scrollEnabled={allowScroll}
             showsHorizontalScrollIndicator={false}
             style={s.form}>
