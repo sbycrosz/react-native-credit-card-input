76f7e981eb12f221b18f2ea266258224c7e55e67
jhes <ooxfordck@gmail.com>
2017-03-16 16:08:01 +0800

    update config

--- example/index.ios.js
+++
@@ -1,4 +0,0 @@
-import Example from "./Example";
-import { AppRegistry } from "react-native";
-
-AppRegistry.registerComponent("example", () => Example);
---
+++ sample/index.ios.js
@@ -0,0 +1,5 @@
+import Example from "./Example";
+import React, {
+  AppRegistry,
+} from 'react-native';
+AppRegistry.registerComponent("sample", () => Example);
