f1e16b821351eb5f81c79e1e989c5608d6fc580e
Alexander Stokes <stokesbga@gmail.com>
2017-06-18 10:55:33 -0400

    export icons

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import connectToState from "./connectToState";
 import CCF from "./CreditCardInput";
 import LiteCCF from "./LiteCreditCardInput";
 import CV from "./CardView";
+import CCIcons from "./Icons";
 
 export const CreditCardInput = connectToState(CCF);
 export const LiteCreditCardInput = connectToState(LiteCCF);
 export const CardView = CV;
+export const CCIcons;
