27dd09287c4733dcffa1a1889df53b2edeadcf1f
Sam Aryasa <sambya@aryasa.net>
2017-12-22 23:52:06 +0800

    Remove native code example

--- example/index.android.js
+++
@@ -1,7 +0,0 @@
-import Example from "./Example";
-import { AppRegistry, UIManager } from "react-native";
-
-// Enable LayoutAnimation on Android
-UIManager.setLayoutAnimationEnabledExperimental(true);
-
-AppRegistry.registerComponent("example", () => Example);
