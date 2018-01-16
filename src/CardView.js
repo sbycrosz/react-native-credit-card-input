9ccafda011dc1c35b3196a901b4c1133aab06fc2
Sébastien Lorber <slorber@users.noreply.github.com>
2018-01-16 15:47:04 +0100

    Add new month/year label props to cardView

--- src/CardView.js
+++ src/CardView.js
@@ -92,6 +92,9 @@ export default class CardView extends Component {
     imageFront: PropTypes.number,
     imageBack: PropTypes.number,
     customIcons: PropTypes.object,
+
+    labelMonth: PropTypes.string,
+    labelYear: PropTypes.string,
   };
 
   static defaultProps = {
@@ -107,12 +110,17 @@ export default class CardView extends Component {
     fontFamily: Platform.select({ ios: "Courier", android: "monospace" }),
     imageFront: require("../images/card-front.png"),
     imageBack: require("../images/card-back.png"),
+
+    labelMonth: "MONTH",
+    labelYear: "YEAR",
   };
 
   render() {
     const { focused,
       brand, name, number, expiry, cvc, customIcons,
-      placeholder, imageFront, imageBack, scale, fontFamily } = this.props;
+      placeholder, imageFront, imageBack, scale, fontFamily,
+      labelMonth, labelYear,
+    } = this.props;
 
     const Icons = { ...defaultIcons, ...customIcons };
     const isAmex = brand === "american-express";
@@ -145,7 +153,7 @@ export default class CardView extends Component {
                 { !name ? placeholder.name : name.toUpperCase() }
               </Text>
               <Text style={[s.baseText, { fontFamily }, s.expiryLabel, s.placeholder, focused === "expiry" && s.focused]}>
-                MONTH/YEAR
+                {labelMonth}/{labelYear}
               </Text>
               <Text style={[s.baseText, { fontFamily }, s.expiry, !expiry && s.placeholder, focused === "expiry" && s.focused]}>
                 { !expiry ? placeholder.expiry : expiry }
