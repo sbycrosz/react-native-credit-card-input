c11e614fc6b8e87ec002afdfc32e6539df9212db
Andrew E. Rhyne <rhyneandrew@gmail.com>
2018-02-19 13:34:55 -0800

    Update CCInput.js

--- src/CCInput.js
+++ src/CCInput.js
@@ -9,6 +9,8 @@ import {
   ViewPropTypes,
 } from "react-native";
 
+const viewPropTypes = ViewPropTypes || View.propTypes;
+
 const s = StyleSheet.create({
   baseInputStyle: {
     color: "black",
@@ -25,7 +27,7 @@ export default class CCInput extends Component {
 
     status: PropTypes.oneOf(["valid", "invalid", "incomplete"]),
 
-    containerStyle: ViewPropTypes.style,
+    containerStyle: viewPropTypes.style,
     inputStyle: Text.propTypes.style,
     labelStyle: Text.propTypes.style,
     validColor: PropTypes.string,
