0f43ac6b6bbd5bf6acf6eb708d7ca3b87e7aa17c
Kuldeep Grewal <kuldeep@punchh.com>
2018-05-05 20:31:48 +0530

    update CardView

--- src/CardView.js
+++ src/CardView.js
@@ -19,15 +19,15 @@ const s = StyleSheet.create({
     resizeMode: 'contain'
   },
   baseText: {
-    color: 'rgba(255, 255, 255, 0.8)',
+    color: 'rgba(100, 0, 0, 0.8)',
     backgroundColor: 'transparent'
   },
   placeholder: {
-    color: 'rgba(255, 255, 255, 0.5)'
+    color: 'rgba(0, 0, 0, 0.5)'
   },
   focused: {
     fontWeight: 'bold',
-    color: 'rgba(255, 255, 255, 1)'
+    color: 'rgba(0, 0, 0, 1)'
   },
   number: {
     fontSize: 21,
