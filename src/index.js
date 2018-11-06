5228c7bf1d07b50dbc24aaffc7783696428656c7
7laria <mosca.ilaria.carmen@gmail.com>
2018-11-06 16:55:01 +0100

    Vertical Form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import connectToState from "./connectToState";
 import CCF from "./CreditCardInput";
 import LiteCCF from "./LiteCreditCardInput";
+import VerticalCCF from "./VerticalCreditCardInput"
 import CV from "./CardView";
 
 export const CreditCardInput = connectToState(CCF);
 export const LiteCreditCardInput = connectToState(LiteCCF);
+export const VerticalCreditCardInput = connectToState(VerticalCCF)
 export const CardView = CV;
