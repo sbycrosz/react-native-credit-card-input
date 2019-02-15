c38423fd5da59da6a29761f217db69e6a00549fa
Paulo Roberto Rosa <itspauloroberto@gmail.com>
2019-02-14 22:03:08 -0200

    added property to activate vertical form from PR #132 from @lauborges

--- src/CreditCardInput.js
+++ src/CreditCardInput.js
@@ -25,6 +25,10 @@ const s = StyleSheet.create({
   inputContainer: {
     marginLeft: 20,
   },
+  verticalInputContainer: {
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
   inputLabel: {
     fontWeight: "bold",
   },
@@ -64,7 +68,8 @@ export default class CreditCardInput extends Component {
     cardBrandIcons: PropTypes.object,
 
     allowScroll: PropTypes.bool,
-
+    verticalInput: PropTypes.bool,
+    
     additionalInputsProps: PropTypes.objectOf(PropTypes.shape(TextInput.propTypes)),
   };
 
@@ -120,7 +125,7 @@ export default class CreditCardInput extends Component {
       inputStyle, labelStyle, validColor, invalidColor, placeholderColor,
       placeholders, labels, values, status,
       onFocus, onChange, onBecomeEmpty, onBecomeValid,
-      additionalInputsProps,
+      additionalInputsProps, verticalInput
     } = this.props;
 
     return {
@@ -163,28 +168,28 @@ export default class CreditCardInput extends Component {
           cvc={cvc}
           onCardLabels={onCardLabels} />
         <ScrollView ref="Form"
-          horizontal
+          horizontal={!verticalInput}
           keyboardShouldPersistTaps="always"
           scrollEnabled={allowScroll}
           showsHorizontalScrollIndicator={false}
           style={s.form}>
           <CCInput {...this._inputProps("number")}
             keyboardType="numeric"
-            containerStyle={[s.inputContainer, inputContainerStyle, { width: CARD_NUMBER_INPUT_WIDTH }]} />
+            containerStyle={[verticalInput ? s.verticalInputContainer : s.inputContainer, inputContainerStyle, { width: CARD_NUMBER_INPUT_WIDTH }]} />
           <CCInput {...this._inputProps("expiry")}
             keyboardType="numeric"
-            containerStyle={[s.inputContainer, inputContainerStyle, { width: EXPIRY_INPUT_WIDTH }]} />
+            containerStyle={[verticalInput ? s.verticalInputContainer : s.inputContainer, inputContainerStyle, { width: EXPIRY_INPUT_WIDTH }]} />
           { requiresCVC &&
             <CCInput {...this._inputProps("cvc")}
               keyboardType="numeric"
-              containerStyle={[s.inputContainer, inputContainerStyle, { width: CVC_INPUT_WIDTH }]} /> }
+              containerStyle={[verticalInput ? s.verticalInputContainer : s.inputContainer, inputContainerStyle, { width: CVC_INPUT_WIDTH }]} /> }
           { requiresName &&
             <CCInput {...this._inputProps("name")}
-              containerStyle={[s.inputContainer, inputContainerStyle, { width: NAME_INPUT_WIDTH }]} /> }
+              containerStyle={[verticalInput ? s.verticalInputContainer : s.inputContainer, inputContainerStyle, { width: NAME_INPUT_WIDTH }]} /> }
           { requiresPostalCode &&
             <CCInput {...this._inputProps("postalCode")}
               keyboardType="numeric"
-              containerStyle={[s.inputContainer, inputContainerStyle, { width: POSTAL_CODE_INPUT_WIDTH }]} /> }
+              containerStyle={[verticalInput ? s.verticalInputContainer : s.inputContainer, inputContainerStyle, { width: POSTAL_CODE_INPUT_WIDTH }]} /> }
         </ScrollView>
       </View>
     );
